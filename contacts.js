7228e75bb1415cac07279548af48281475a74e3c
VictoriaSzn <victoriia.szn@gmail.com>
2023-05-05 20:27:31 +0300

    1

--- contacts.js
+++ contacts.js
@@ -5,25 +5,22 @@ const pach = require("path");
 const { nanoid } = require("nanoid");
 
 const contactsPach = pach.join(__dirname, "db", "contacts.json");
-//для yargs
 
 const updateContact =  async (contacts)=>await fs.writeFile(contactsPach, JSON.stringify(contacts, null, 2));
-////замена повторяющейся строки кода
 
-const getAll = async () => {
+const listContacts = async () => {
     const readFile = await fs.readFile(contactsPach);
     return JSON.parse(readFile);
 }
 
-const getById = async (id) => {
-    //const contactsId = String(id);
-    const contacts = await getAll();
-     const result = contacts.find(item=> item.id === id); // (item=> item.id === contactsId)
+const getContactById = async (id) => {
+     const contacts = await listContacts();
+     const result = contacts.find(item=> item.id === id); 
     return result||null;
  }
 
-const add = async (data) => {
-    const contacts = await getAll();
+const addContact = async (data) => {
+    const contacts = await listContacts();
     const newContact = {
         id:nanoid(),
         ...data,
@@ -31,39 +28,23 @@ const add = async (data) => {
     }
     contacts.push(newContact);
     await updateContact(contacts);
-    ////await fs.writeFile(contactsPach, JSON.stringify(contacts, null, 2));
     return newContact;
 }
  
-const updateById = async (id, data) => {
-    //const contactsId = String(id);
-    const contacts = await getAll();
-    const index = contacts.findIndex(item => item.id === id); // (item=> item.id === contactsId)
-    if (index === -1) {
-        return null;
-    }
-    contacts[index] = { id, ...data };
-    ////await fs.writeFile(contactsPach, JSON.stringify(contacts, null, 2));
-    await updateContact(contacts);
-    return contacts[index];
-}
-const deleteById = async (id) => {
-    //const contactsId = String(id);
-    const contacts = await getAll();
+const removeContact = async (id) => {
+    const contacts = await listContacts();
      const index = contacts.findIndex(item=> item.id === id); // (item=> item.id === contactsId)
     if (index === -1) {
         return null;
     }
     const [result] = contacts.splice(index, 1);
-    //await fs.writeFile(contactsPach, JSON.stringify(contacts, null, 2));
     await updateContact(contacts);
     return result;
 }
 
 module.exports = {
-    getAll,
-    getById,
-    add,
-    updateById,
-    deleteById
+    listContacts,
+    getContactById,
+    addContact,
+    removeContact
 }
--- index.js
+++ index.js
@@ -1,48 +1,31 @@
 const contacts = require("./contacts");
-//const yargs = require("yargs");
-//const { hideBin } = require("yargs/helpers");
 const { program } = require("commander");
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
     switch (action) {
-        case "read":
-            const allContacts = await contacts.getAll();
+        case "list":
+            const allContacts = await contacts.listContacts();
             return console.log(allContacts);
-        case "getById":
-            const oneContact = await contacts.getById(id);
+        case "get":
+            const oneContact = await contacts.getContactById(id);
             return console.log(oneContact);
         case "add":
-            const newContact = await contacts.add({ name, email, phone });
+            const newContact = await contacts.addContact({ name, email, phone });
             return console.log(newContact);
-        case "updateById":
-            const updateContact = await contacts.updateById(id, { name, email, phone });
-            return console.log(updateContact);
-        case "deleteById":
-            const deleteContact = await contacts.deleteById(id);
+        case "remove":
+            const deleteContact = await contacts.removeContact(id);
             return console.log(deleteContact);
         default:
-            return console.log("Uncnown action");
+            console.warn("\x1B[31m Unknown action type!");
     }
 }
-//invokeAction({ action: "read" });
-//invokeAction({ action: "getById", id: "rsKkOQUi80UsgVPCcLZZW" });
-//invokeAction({ action: "add", name: "Vasia", email: "15@ukr.net", phone: "555-888" });
-//invokeAction({ action: "updateById", id: "x3v1ckiH4_vAyKYJcy_GY", name: "Vasiliy", email: "15@ukr.net", phone: "555-888" });
-//invokeAction({ action: "deleteById", id: "x3v1ckiH4_vAyKYJcy_GY"});
-
-//для yargs
-// const arr = hideBin(process.argv);
-// const { argv } = yargs(arr);
-// invokeAction(argv);
-
-//для commander
 program
     .option("-a, --action, <type>")
     .option("-i, --id, <type>")
     .option("-n, --name, <type>")
     .option("-e, --email, <type>")
     .option("-p, --phone, <type>");
-program.parse();//раніше треба було так program.parse(process.argv);
+program.parse();
 const options = program.opts(); 
 
 invokeAction(options);
